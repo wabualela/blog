93bfda70a43abbdd82a3c14d1cb66f031e573933
wabualela <wailabualela@gmail.com>
2021-08-19 11:51:45 +0200

    posts

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ config('app.name') }}</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+
+<body>
+    <h1>Posts</h1>
+
+    <article>
+        <h3><a href="/posts/my-first-post">My First Article</a></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi perferendis neque delectus rem quaerat aspernatur animi eum, laborum facilis sunt a, consectetur dolorum necessitatibus velit quasi autem omnis adipisci quod?</p>
+    </article>
+
+    <article>
+        <h3><a href="/posts/my-second-post">My Second Article</a></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et laborum optio vel nulla reprehenderit ipsam quis cumque similique! Dolorem possimus totam harum dicta aliquid, hic sapiente repudiandae eveniet. Blanditiis, neque?</p>
+    </article>
+
+    <article>
+        <h3><a href="/posts/my-third-post">My Third Article</a></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam rerum, iusto sunt porro quae corrupti veniam alias quod libero qui necessitatibus velit ad assumenda id quisquam dolor vel est eum!</p>
+    </article>
+
+</body>
+
+</html>
