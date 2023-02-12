6fb89437f4a5095306cd20d04d1911e3b000e7d0
wabualela <wailbualela@gmail.com>
2023-02-12 17:44:42 +0200

    add the theme & extract comps

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,37 +1,20 @@
-<!DOCTYPE html>
-<html lang="ar" dir="rtl">
+<x-layout>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include('_post-header')
 
-    <title>{{ config('app.name') }}</title>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <x-post-featured-card />
 
-    <style>
-        body {
-            text-align: right;
-        }
-    </style>
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
 
-</head>
-
-<body>
-    <main>
-        <h1>مقالات</h1>
-
-        @foreach($posts as $post)
-        <article>
-            <h3>
-                <a href="{{ url('posts/' .$post->slug) }}">{{ $post->title }}</a>
-                <h4>{{$post->category->name}}</h4>
-            </h3>
-            <p>{{ $post->body }}</p>
-        </article>
-        @endforeach
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
     </main>
-</body>
-
-</html>
+</x-layout>
